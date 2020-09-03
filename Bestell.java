d58504c47ac75af078289cdf0e27e1cf07d4958a
Distrip1 <62309993+Distrip1@users.noreply.github.com>
2020-09-03 20:31:36 +0200

    Add files via upload

--- Bestell.java
+++ Bestell.java
@@ -1,12 +1,12 @@
 import java.util.*;
 public class Bestell {
     public static void main(String argv[]){
+        System.out.print("Programm zur Berechnung des Rechnungsbetrags \n Bestellwert in Euro: ");
         double b = rechnung();
         System.out.println(" Rechnungsbetrag: " + b + "\n Programmende.");
     }
     public static double rechnung(){
         Scanner ein = new Scanner(System.in);
-        System.out.print("Programm zur Berechnung des Rechnungsbetrags \n Bestellwert in Euro: ");
         double b = ein.nextDouble();
         if(b < 200 && b > 0)
             b = b + 5.5;
